fa424197591790f952aabefa8fbf9c17f103b387
Witoon <alive-@msn.com>
2015-05-08 12:33:17 +0700

    Add new folder, Config bootstrapper to use SimpleContainer

--- AppBootstrapper.cs
+++ AppBootstrapper.cs
@@ -9,9 +9,33 @@ namespace TirkxDownloader
 {
     class AppBootstrapper : BootstrapperBase
     {
+        private SimpleContainer container;
+
         public AppBootstrapper()
         {
             Initialize();
+        }
+
+        protected override void Configure()
+        {
+            container = new SimpleContainer();
+            container.Singleton<IWindowManager, WindowManager>();
+            container.Singleton<IEventAggregator, EventAggregator>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            container.BuildUp(instance);
         }
     }
 }
