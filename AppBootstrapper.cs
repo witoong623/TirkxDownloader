b36c44bbe57661a2bd97bd3f4ad2e1addf3629a8
Witoon <alive-@msn.com>
2015-05-08 10:35:58 +0700

    Initial

---
+++ AppBootstrapper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+
+namespace TirkxDownloader
+{
+    class AppBootstrapper : BootstrapperBase
+    {
+        public AppBootstrapper()
+        {
+            Initialize();
+        }
+    }
+}
