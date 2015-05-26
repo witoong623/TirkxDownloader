cb08b1ce1b35372873fad5a5893ab7a0cca8d92b
Witoon <alive-@msn.com>
2015-05-26 18:15:08 +0700

    Revert "Delete paser context"
    This reverts commit a0e81d437df3287a84eedb8c7af0af9b74929e20.

---
+++ Framework/Anime.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TirkxDownloader.Models;
+
+namespace TirkxDownloader.Framework
+{
+    public class Anime
+    {
+        private string threadLink;
+        private DateTime threadStartDate;
+
+        public string Name { get; set; }
+        public Dictionary<string, List<AnimeFileInfo>> DownloadLink { get; set; }
+
+        public string ThreadLink
+        {
+            get { return threadLink; }
+            set
+            {
+                threadLink = TirkxDownloadParser.ForumLink + value;
+            }
+        }
+
+        public DateTime ThreadStartDate
+        {
+            get { return threadStartDate; }
+            set
+            {
+                threadStartDate = value;
+            }
+        }
+
+        public Anime()
+        {
+            DownloadLink = new Dictionary<string, List<AnimeFileInfo>>();
+        }
+    }
+}
---
+++ Framework/FileInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TirkxDownloader.Framework
+{
+    public class AnimeFileInfo
+    {
+        public string FileName { get; set; }
+        public string Link { get; set; }
+    }
+}
