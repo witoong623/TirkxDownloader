7b68238a4e4083eeffcec35c0b7ea700dc629456
Witoon <alive-@msn.com>
2015-05-27 11:26:34 +0700

    Update DownloadInfo

---
+++ Framework/DownloadInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TirkxDownloader.Framework
+{
+    public enum DownloadStatus { Queue, Complete, Error }
+
+    public class DownloadInfo
+    {
+        public string FileName { get; set; }
+        public string DownloadLink { get; set; }
+        public string SaveDestination { get; set; }
+        public DateTime AddDate { get; set; }
+        public DateTime CompleteDate { get; set; }
+        public DownloadStatus Status { get; set; }
+    }
+}
--- ViewModels/QueueViewModel.cs
+++ ViewModels/QueueViewModel.cs
@@ -3,15 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 using Caliburn.Micro;
+using TirkxDownloader.Framework;
 
 namespace TirkxDownloader.ViewModels
 {
     class QueueViewModel : Screen, IContentList
     {
+        private BindableCollection<DownloadInfo> downloadInfoList;
+        private DownloadInfo selectedItem;
+
+        public BindableCollection<DownloadInfo> DownloadInfoList
+        {
+            get { return downloadInfoList; }
+        }
+
+        public DownloadInfo SelectedItem
+        {
+            get { return selectedItem; }
+            set
+            {
+                selectedItem = value;
+                NotifyOfPropertyChange(() => SelectedItem);
+            }
+        }
+
         public QueueViewModel()
         {
             DisplayName = "Queue/Downloading";
+            downloadInfoList = new BindableCollection<DownloadInfo>();
+        }
+
+        protected override void OnInitialize()
+        {
+            for (int i = 0; i < 30; i++)
+            {
+                downloadInfoList.Add(new DownloadInfo { FileName = "School days", DownloadLink = "tirkx download thread",
+                    SaveDestination = "D drive", AddDate = DateTime.Now });
+            }
+        }
+
+        public void SelectItem(System.Windows.Controls.ListView listView)
+        {
+            SelectedItem = (DownloadInfo)listView.SelectedItem;
         }
     }
 }
