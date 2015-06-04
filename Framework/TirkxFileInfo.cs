4b0b9b8611d9edeab137048d46957d090b4c0bf1
Witoon <alive-@msn.com>
2015-06-04 18:58:04 +0700

    Add TirkxFileInfo class

---
+++ Framework/TirkxFileInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace TirkxDownloader.Framework
+{
+    public class TirkxFileInfo
+    {
+        [JsonProperty("FileName")]
+        public string FileName { get; private set; }
+        [JsonProperty("DownloadLink")]
+        public string DownloadLink { get; private set; }
+    }
+}
