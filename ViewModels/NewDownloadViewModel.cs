9c0dd715d351c1475023324a2d8f6cb362b67993
Witoon <alive-@msn.com>
2015-06-20 21:43:47 +0700

    Update NewDownloadViewModel

--- ViewModels/NewDownloadViewModel.cs
+++ ViewModels/NewDownloadViewModel.cs
@@ -1,28 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using Caliburn.Micro;
 using Microsoft.WindowsAPICodePack.Dialogs;
 using MahApps.Metro.Controls;
+using TirkxDownloader.Models;
 using TirkxDownloader.Framework;
 
 namespace TirkxDownloader.ViewModels
 {
     public class NewDownloadViewModel : Screen
     {
+        private readonly DownloadEngine Engine;
         private readonly IWindowManager WindowManager;
         private readonly IEventAggregator EventAggregator;
         private MetroWindow View;
 
         public DownloadInfo CurrentItem { get; private set; }
 
+        public bool CanQueue
+        {
+            get { return Directory.Exists(CurrentItem.SaveLocation); }
+        }
+
+        public bool CanDownload
+        {
+            get { return CanQueue; }
+        }
+
         public NewDownloadViewModel(IWindowManager windowManager,
-            IEventAggregator eventAggregator, TirkxFileInfo fileInfo)
+            IEventAggregator eventAggregator, TirkxFileInfo fileInfo, DownloadEngine engine)
         {
             WindowManager = windowManager;
             EventAggregator = eventAggregator;
+            Engine = engine;
             DisplayName = "New download file";
             View = (MetroWindow)GetView();
 
@@ -42,14 +53,16 @@ namespace TirkxDownloader.ViewModels
             {
                 CurrentItem.SaveLocation = folderBrowser.FileName;
                 NotifyOfPropertyChange(() => CurrentItem);
+                NotifyOfPropertyChange(() => CanDownload);
+                NotifyOfPropertyChange(() => CanQueue);
             }
         }
 
         public void Download()
         {
-            CurrentItem.Status = DownloadStatus.Downloading;
             CurrentItem.AddDate = DateTime.Now;
             EventAggregator.PublishOnUIThread(CurrentItem);
+            Engine.StartDownload(CurrentItem);
 
             var window = (MetroWindow)GetView();
             window.Close();
@@ -57,7 +70,6 @@ namespace TirkxDownloader.ViewModels
 
         public void Queue()
         {
-            CurrentItem.Status = DownloadStatus.Queue;
             CurrentItem.AddDate = DateTime.Now;
             EventAggregator.PublishOnUIThread(CurrentItem);
 
@@ -69,6 +81,13 @@ namespace TirkxDownloader.ViewModels
         {
             var window = (MetroWindow)GetView();
             window.Close();
+        }
+
+        protected override void OnActivate()
+        {
+            base.OnActivate();
+            var window = (MetroWindow)GetView();
+            window.Focus();
         }
     }
 }
