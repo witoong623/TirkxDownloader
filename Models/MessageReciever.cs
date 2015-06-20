5674cfaab40158e3e69fb5cd63ca006e84e78b16
Witoon <alive-@msn.com>
2015-06-20 21:41:50 +0700

    Change recieve method

--- Models/MessageReciever.cs
+++ Models/MessageReciever.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
-using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using Caliburn.Micro;
 using Newtonsoft.Json;
 using TirkxDownloader.ViewModels;
@@ -15,14 +13,16 @@ namespace TirkxDownloader.Models
 {
     public class MessageReciever
     {
+        private readonly DownloadEngine Engine;
         private readonly IEventAggregator EventAggregator;
         private readonly IWindowManager WindowManager;
         private Thread BackgroundThread;
 
-        public MessageReciever(IWindowManager windowManager, IEventAggregator eventAggregator)
+        public MessageReciever(IWindowManager windowManager, IEventAggregator eventAggregator, DownloadEngine engine)
         {
             WindowManager = windowManager;
             EventAggregator = eventAggregator;
+            Engine = engine;
             BackgroundThread = new Thread(StartReciever);
         }
 
@@ -32,33 +32,21 @@ namespace TirkxDownloader.Models
             {
                 var fileInfo = GetFileInfo();
                 Execute.OnUIThread(() => WindowManager.ShowDialog(
-                    new NewDownloadViewModel(WindowManager, EventAggregator, fileInfo)));
+                    new NewDownloadViewModel(WindowManager, EventAggregator, fileInfo, Engine)));
             }
         }
 
         private TirkxFileInfo GetFileInfo()
         {
-            using (var stdin = Console.OpenStandardInput())
+            using (var listener = new HttpListener())
             {
-                int msgLength = 0;
+                listener.Prefixes.Add("http://localhost:6230/");
+                listener.Start();
+                var requestContext = listener.GetContext();
+                var streamReader = new StreamReader(requestContext.Request.InputStream, requestContext.Request.ContentEncoding);
+                string jsonString = streamReader.ReadToEnd();
 
-                while (true)
-                {
-                    byte[] sizeBuffer = new byte[4];
-                    stdin.Read(sizeBuffer, 0, 4);
-                    msgLength = BitConverter.ToInt32(sizeBuffer, 0);
-
-                    if (msgLength > 0)
-                    {
-                        break;
-                    }
-                }
-
-                byte[] msgBuffer = new byte[msgLength];
-                stdin.Read(msgBuffer, 0, msgLength);
-                var jsonText = Encoding.UTF8.GetString(msgBuffer);
-
-                return JsonConvert.DeserializeObject<TirkxFileInfo>(jsonText);
+                return JsonConvert.DeserializeObject<TirkxFileInfo>(jsonString);
             }
         }
 
