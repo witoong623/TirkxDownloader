4d7e1263fc28ae5f1a1c48fdd636f65fcad3c847
Witoon <alive-@msn.com>
2015-06-20 21:44:47 +0700

    Add CounterWarpper to use in separate thread

---
+++ Framework/CounterWarpper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace TirkxDownloader.Framework
+{
+    public class CounterWarpper
+    {
+        private int counter;
+
+        public int Counter { get { return counter; } }
+
+        public void Increase()
+        {
+            Interlocked.Increment(ref counter);
+        }
+
+        public void Decrease()
+        {
+            Interlocked.Decrement(ref counter);
+        }
+    }
+}
