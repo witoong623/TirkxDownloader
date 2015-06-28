6891f141b4d9adea4763bb21ffd3a92ef9079807
Witoon <alive-@msn.com>
2015-06-28 08:33:25 +0700

    Delete SingleInstance because we don't need it anymore

--- App.xaml.cs
+++ App.xaml.cs
@@ -12,32 +12,8 @@ namespace TirkxDownloader
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application, ISingleInstanceApp
+    public partial class App : Application
     {
-        private const string Unique = "TirkxDownloader";
 
-        [STAThread]
-        public static void Main()
-        {
-            if (SingleInstance<App>.InitializeAsFirstInstance(Unique))
-            {
-                var application = new App();
-
-                application.InitializeComponent();
-                application.Run();
-
-                // Allow single instance code to perform cleanup operations
-                SingleInstance<App>.Cleanup();
-            }
-        }
-
-        #region ISingleInstanceApp Members
-
-        public bool SignalExternalCommandLineArgs(IList<string> args)
-        {
-            return true;
-        }
-
-        #endregion
-        }
+    }
 }
