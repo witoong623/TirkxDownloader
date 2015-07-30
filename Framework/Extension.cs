ecb6c3cb36c4cd788d6d8f1a2e79b5dd695ff7e3
Witoon <alive-@msn.com>
2015-07-30 17:22:18 +0700

    Added Pair generic class to collect 2 Types in Dictionary and GetRespone that accept CancellationToken

---
+++ Framework/Extension.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TirkxDownloader.Framework
+{
+    public static class Extension
+    {
+        public static async Task<HttpWebResponse> GetResponseAsync(this HttpWebRequest request, CancellationToken ct)
+        {
+            using (ct.Register(() => request.Abort(), useSynchronizationContext: false))
+            {
+                try
+                {
+                    var response = await request.GetResponseAsync();
+                    ct.ThrowIfCancellationRequested();
+
+                    return (HttpWebResponse)response;
+                }
+                catch (WebException webEx)
+                {
+                    if (ct.IsCancellationRequested)
+                    {
+                        throw new OperationCanceledException(webEx.Message, webEx, ct);
+                    }
+
+                    throw;
+                }
+            }
+        }
+    }
+}
---
+++ Framework/Pair.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TirkxDownloader.Framework
+{
+    public sealed class Pair<TFirst, TSecond> : IEquatable<Pair<TFirst, TSecond>>
+    {
+        private readonly TFirst first;
+        private readonly TSecond second;
+
+        public Pair(TFirst first, TSecond second)
+        {
+            this.first = first;
+            this.second = second;
+        }
+
+        public TFirst First
+        {
+            get { return first; }
+        }
+
+        public TSecond Second
+        {
+            get { return second; }
+        }
+
+        public override bool Equals(Pair<TFirst, TSecond> other)
+        {
+            if (other == null)
+            {
+                return false;
+            }
+
+            return EqualityComparer<TFirst>.Default.Equals(first, other.first) &&
+                EqualityComparer<TSecond>.Default.Equals(second, other.second);
+        }
+
+        public override int GetHashCode()
+        {
+            return EqualityComparer<TFirst>.Default.GetHashCode(first) * 37 +
+               EqualityComparer<TSecond>.Default.GetHashCode(second);
+        }
+    }
+}
