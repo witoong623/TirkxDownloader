d770342a5f18ad633799ba426b2e62d50f51d9ad
Witoon <alive-@msn.com>
2015-08-11 12:09:23 +0700

    Add SettingShellViewModel to use for setting, create NewWindowViewModel folder to store all new window

---
+++ ViewModels/NewWindowViewModel/SettingShellViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+using MahApps.Metro.Controls;
+using TirkxDownloader.Framework;
+using TirkxDownloader.Models;
+
+
+namespace TirkxDownloader.ViewModels
+{
+    public class SettingShellViewModel : Conductor<IContentList>.Collection.OneActive
+    {
+    }
+}
