82f67d97e7c781c96bc5ae43af9db707c6a2959b
Witoon <alive-@msn.com>
2015-08-13 21:52:57 +0700

    Initial new viewmodel for setting

---
+++ ViewModels/NewWindowViewModels/AuthorizationViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+
+namespace TirkxDownloader.ViewModels
+{
+    public class AuthorizationViewModel : Screen, IContentList
+    {
+        public AuthorizationViewModel()
+        {
+            DisplayName = "Authorization";
+        }
+
+        public bool IsEmpty { get { return false; } }
+    }
+}
