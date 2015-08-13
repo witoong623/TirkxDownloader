b1e900dfffeabcecd6849026b9fa7458956eeed9
Witoon <alive-@msn.com>
2015-08-13 21:39:17 +0700

    Add AuthorizationStore to manage credentials for each target host

---
+++ Framework/AuthorizationInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TirkxDownloader.Framework
+{
+    public class AuthorizationInfo
+    {
+        public string TargetName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Models/AuthorizationStore.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CredentialManagement;
+using TirkxDownloader.Framework;
+
+namespace TirkxDownloader.Models
+{
+    public class AuthorizationStore
+    {
+        public AuthorizationInfo GetCredential(string target)
+        {
+            var credential = new Credential { Target = target };
+
+            if (!credential.Load())
+            {
+                return null;
+            }
+
+            return new AuthorizationInfo
+            {
+                Username = credential.Username,
+                Password = credential.Password,
+                TargetName = credential.Target
+            };
+        }
+
+        public IList<AuthorizationInfo> GetAllCredential()
+        {
+            var domains = new List<string>();
+
+            using (var str = new StreamReader("Target domain", Encoding.UTF8))
+            {
+                string line;
+
+                while ((line = str.ReadLine()) != null)
+                {
+                    domains.Add(line);
+                }
+            }
+
+            var credentials = new List<AuthorizationInfo>();
+            AuthorizationInfo authorization;
+
+            foreach (var domain in domains)
+            {
+                if ((authorization = GetCredential(domain)) != null)
+                {
+                    credentials.Add(authorization);
+                }
+            }
+
+            return credentials;
+        }
+
+        public bool SaveCredential(string target, string username, string password)
+        {
+            using (var stw = new StreamWriter("Target domain", true, Encoding.UTF8))
+            {
+                stw.WriteLine(target);
+            }
+
+            var credential = new Credential
+            {
+                Target = target,
+                PersistanceType = PersistanceType.Enterprise,
+                Username = username,
+                Password = password
+            };
+
+            return credential.Save();
+        }
+
+        public bool DeleteCredential(string target)
+        {
+            var credential = new Credential { Target = target };
+
+            return credential.Delete();
+        }
+    }
+}
