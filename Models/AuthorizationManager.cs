8414713a62203aff07a4dfda360174ace3b9faf4
Witoon <alive-@msn.com>
2015-09-02 21:42:57 +0700

    Check targetDomain empty, change file extension to .dat, check file existing, re-implement  save method and delete method

--- Models/AuthorizationManager.cs
+++ Models/AuthorizationManager.cs
@@ -11,9 +11,19 @@ namespace TirkxDownloader.Models
 {
     public class AuthorizationManager
     {
+        /// <summary>
+        /// get credential
+        /// </summary>
+        /// <param name="target">target site hostname</param>
+        /// <returns>AuthorizationInfo that contain credential informations, null when not found</returns>
         public AuthorizationInfo GetCredential(string target)
         {
             var credential = new Credential { Target = target };
+
+            if (string.IsNullOrEmpty(target))
+            {
+                return null;
+            }
 
             if (!credential.Load())
             {
@@ -33,12 +43,12 @@ namespace TirkxDownloader.Models
             var domains = new List<string>();
             var credentials = new List<AuthorizationInfo>();
 
-            if (!File.Exists("Target domain"))
+            if (!File.Exists("Target domain.dat"))
             {
                 return credentials;
             }
 
-            using (var str = new StreamReader("Target domain", Encoding.UTF8))
+            using (var str = new StreamReader("Target domain.dat", Encoding.UTF8))
             {
                 string line;
 
@@ -64,7 +74,24 @@ namespace TirkxDownloader.Models
 
         public bool SaveCredential(string target, string username, string password)
         {
-            using (var stw = new StreamWriter("Target domain", true, Encoding.UTF8))
+            // in case this target was added before
+            if (File.Exists("Target domain.dat"))
+            {
+                using (var str = new StreamReader("Target domain.dat", Encoding.UTF8))
+                {
+                    string temp;
+
+                    while ((temp = str.ReadLine()) != null)
+                    {
+                        if (target.Equals(temp))
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+
+            using (var stw = new StreamWriter("Target domain.dat", true, Encoding.UTF8))
             {
                 stw.WriteLine(target);
             }
@@ -77,14 +104,41 @@ namespace TirkxDownloader.Models
                 Password = password
             };
 
+            // this maybe return false if credential has been added
             return credential.Save();
         }
 
         public bool DeleteCredential(string target)
         {
             var credential = new Credential { Target = target };
+            bool flag = credential.Delete();
 
-            return credential.Delete();
+            if (flag)
+            {
+                var lines = new List<string>();
+
+                using (var str = new StreamReader("Target domain.dat", Encoding.UTF8))
+                {
+                    string temp;
+
+                    while ((temp = str.ReadLine()) != null)
+                    {
+                        lines.Add(temp);
+                    }
+                }
+
+                lines.Remove(target);
+
+                using (var stw = new StreamWriter("Target domain.dat", false, Encoding.UTF8))
+                {
+                    foreach (var line in lines)
+                    {
+                        stw.WriteLine(line);
+                    }
+                }
+            }
+
+            return flag;
         }
     }
 }
