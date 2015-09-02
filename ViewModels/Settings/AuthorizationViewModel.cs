4096b24fc19c03702d3c854b2f920443fa751390
Witoon <alive-@msn.com>
2015-09-02 22:02:14 +0700

    Add CanXXX method and bind current AuthorizationInfo to View

--- ViewModels/Settings/AuthorizationViewModel.cs
+++ ViewModels/Settings/AuthorizationViewModel.cs
@@ -21,6 +21,7 @@ namespace TirkxDownloader.ViewModels.Settings
         private AuthorizationManager _authorization;
 
         public BindableCollection<AuthorizationInfo> Credentials { get; private set; }
+
         public AuthorizationViewModel(AuthorizationManager credentialsStorage)
         {
             _authorization = credentialsStorage;
@@ -35,7 +36,69 @@ namespace TirkxDownloader.ViewModels.Settings
             get { return _currentItem; }
             set
             {
+                _currentItem = value;
                 NotifyOfPropertyChange(nameof(CurrentItem));
+                NotifyOfPropertyChange(nameof(CanDelete));
+                NotifyOfPropertyChange(nameof(CanEdit));
+            }
+        }
+
+        #region summary properties member
+        public bool CanDelete
+        {
+            get { return CurrentItem != null; }
+        }
+
+        public bool CanEdit
+        {
+            get { return CurrentItem != null; }
+        }
+        #endregion
+
+        #region modify properties member
+        private string _username;
+        private string _password;
+        private string _targetName;
+
+        public string TargetName
+        {
+            get { return _targetName; }
+            set
+            {
+                _targetName = value;
+                NotifyOfPropertyChange(nameof(TargetName));
+                NotifyOfPropertyChange(nameof(CanSubmit));
+            }
+        }
+
+        public string Username
+        {
+            get { return _username; }
+            set
+            {
+                _username = value;
+                NotifyOfPropertyChange(nameof(Username));
+                NotifyOfPropertyChange(nameof(CanSubmit));
+            }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                _password = value;
+                NotifyOfPropertyChange(nameof(Password));
+                NotifyOfPropertyChange(nameof(CanSubmit));
+            }
+        }
+
+        public bool CanSubmit
+        {
+            get
+            {
+                return !string.IsNullOrWhiteSpace(Username) && !string.IsNullOrWhiteSpace(Password) &&
+                  !string.IsNullOrWhiteSpace(TargetName);
             }
         }
 
@@ -58,26 +121,77 @@ namespace TirkxDownloader.ViewModels.Settings
                 NotifyOfPropertyChange(nameof(CancelMessage));
             }
         }
+        #endregion
 
+        #region override member
         protected override void OnInitialize()
         {
             DisplayName = "Authorization";
             Credentials.AddRange(_authorization.GetAllCredential());
         }
+        #endregion
 
-        
-
+        #region summary view method
         public void Add()
         {
+            IsSet = true;
             SubmitMessage = SAVESUBMIT;
             CancelMessage = CANCEL;
-            var settingParent = (SettingShellViewModel) Parent;
+            var settingParent = (SettingShellViewModel)Parent;
             settingParent.State = SettingState.Modify;
+        }
+
+        public void Edit()
+        {
+            IsSet = false;
+            SubmitMessage = SAVECHANGE;
+            CancelMessage = UNCHANGE;
+            var settingParent = (SettingShellViewModel)Parent;
+            settingParent.State = SettingState.Modify;
+        }
+
+        public void Delete()
+        {
+            if (_authorization.DeleteCredential(CurrentItem.TargetName))
+            {
+                CurrentItem = null;
+            }
+        }
+
+        public void SelectItem(AuthorizationInfo authorizationInfo)
+        {
+            CurrentItem = authorizationInfo;
+        }
+        #endregion
+
+        #region Modify view method
+        public void Submit()
+        {
+            bool flag = _authorization.SaveCredential(TargetName, Username, Password);
+            var settingParent = (SettingShellViewModel)Parent;
+            settingParent.State = SettingState.Summary;
+
+            if (flag)
+            {
+                Credentials.Add(_authorization.GetCredential(TargetName));
+            }
+
+            CleanTextbox();
         }
 
         public void Cancel()
         {
-            
+            var settingParent = (SettingShellViewModel)Parent;
+            settingParent.State = SettingState.Summary;
+            CleanTextbox();
         }
+
+        private void CleanTextbox()
+        {
+            TargetName = string.Empty;
+            Username = string.Empty;
+            Password = string.Empty;
+        }
+        #endregion
     }
 }
