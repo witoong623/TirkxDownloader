0a550a23091095e98b778ef09987043f8ccc1ff9
Witoon <alive-@msn.com>
2015-09-02 21:39:21 +0700

    Add Link method to compare string with wildcards
    from https://stackoverflow.com/questions/188892/glob-pattern-matching-in-net

--- Framework/Extension.cs
+++ Framework/Extension.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -41,6 +42,20 @@ namespace TirkxDownloader.Framework
             }
 
             return list;
+        }
+
+        /// <summary>
+        /// Compares the string against a given pattern.
+        /// </summary>
+        /// <param name="str">The string.</param>
+        /// <param name="pattern">The pattern to match, where "*" means any sequence of characters, and "?" means any single character.</param>
+        /// <returns><c>true</c> if the string matches the given pattern; otherwise <c>false</c>.</returns>
+        public static bool Like(this string str, string pattern)
+        {
+            return new Regex(
+                "^" + Regex.Escape(pattern).Replace(@"\*", ".*").Replace(@"\?", ".") + "$",
+                RegexOptions.IgnoreCase | RegexOptions.Singleline
+            ).IsMatch(str);
         }
     }
 }
