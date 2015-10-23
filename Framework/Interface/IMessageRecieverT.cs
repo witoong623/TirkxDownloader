f323a4fee886190dd9502945f45f4624ee999178
Witoon <alive-@msn.com>
2015-10-23 21:42:55 +0700

    Add IMessageReciever interface

---
+++ Framework/Interface/IMessageRecieverT.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TirkxDownloader.Framework.Interface
+{
+    /// <summary>
+    /// This interface should be implemented for recieve JSON then parse it and return to caller
+    /// </summary>
+    /// <typeparam name="T">Type of message, must be JSON compatibility</typeparam>
+    interface IMessageReciever<T>
+    {
+        ICollection<string> Prefixes { get; }
+
+        bool IsRecieving { get; }
+
+        Task<T> GetMessageAsync();
+
+        /// <summary>
+        /// Call this method when terminate application
+        /// </summary>
+        void Close();
+
+        /// <summary>
+        /// Call this method when you want to stop reciever
+        /// </summary>
+        void StopReciever();
+    }
+}
