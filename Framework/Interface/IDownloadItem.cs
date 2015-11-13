773e709aa250c357422f4aa379fdd986b6a522cf
Witoon <alive-@msn.com>
2015-11-13 21:41:49 +0700

    Add IDownloadItem for future use

---
+++ Framework/Interface/IDownloadItem.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using NodaTime;
+using System.ComponentModel;
+using System.IO;
+
+namespace TirkxDownloader.Framework.Interface
+{
+    public interface IDownloadItem : INotifyPropertyChanged
+    {
+        #region event
+        event DownloadCompleteHandler DownloadComplete;
+
+        void OnDownloadComplete();
+        #endregion
+
+        #region properties
+        /// <summary>
+        /// File name including extension
+        /// </summary>
+        string FileName { get; set; }
+
+        DownloadStatus Status { get; set; }
+
+        string DownloadLink { get; set; }
+
+        string SaveLocation { get; set; }
+
+        /// <summary>
+        /// Full path including filename and extension
+        /// </summary>
+        string FullName { get; }
+
+        DateTime AddOn { get; set; }
+
+        DateTime? CompleteOn { get; set; }
+
+        /// <summary>
+        /// File size in MB
+        /// </summary>
+        double FileSize { get; set; }
+
+        int Speed { get; set; }
+
+        string ErrorMessage { get; set; }
+
+        double PercentProgress { get; set; }
+
+        /// <summary>
+        /// In MB
+        /// </summary>
+        double RecievedSize { get; set; }
+
+        Duration? ETA { get; set; }
+
+        Stream InStream { get; set; }
+        #endregion
+    }
+}
